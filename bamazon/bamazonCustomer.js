b16c76b805f3f1857a38efa7e55f23f67352b2f7
reducks3000 <reducks3000@gmail.com>
2020-01-14 00:35:56 -0600

    added node functionality to display inventory from the database and basic logic to allow customers to buy products

--- bamazon/bamazonCustomer.js
+++ bamazon/bamazonCustomer.js
@@ -1,28 +1,41 @@
-let mysql = require("mysql");
-let inquirer = require("inquirer");
-require("console.table");
+// Initialize Modules
+const inquirer = require('inquirer');
+const mysql = require('mysql');
+require('console.table');
 
-let connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "",
-    database: "bamazon"
+// Initialize MySQL DB connection
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'bamazon'
 });
 
-connection.connect(function(err) {
-if (err) {
-    console.error("error connecting: " + err.stack)
-}
-loadProducts();
+connection.connect(err => 
+    (err) ? console.log('Error:',err) : 
+    console.log('Connected successfully'));
+
+// Query DB for list of all products to be printed to console as a table
+connection.query('SELECT item_id, product_name, price FROM products', function (err, products){
+    if (err) throw err;
+    console.table(products)
+    
+    //Ask user for what product they wish to buy
+
+    function buyProduct() {
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'chooseProduct',
+                message: 'Choose a product to purchase',
+                choices: function() {
+                    const output = products.map((product) => `${product.item_id}) ${product.product_name}`);
+                    return output;
+                },
+                filter: choice => parseInt(choice.charAt(0)),
+            },
+
+        ])
+    }
+
 });
-
-// function to load product tables
-function loadProducts() {
-    connection.query("SELECT * FROM products", function(err, res) {
-        if (err) throw err;
-
-        console.table(res);
-    });
-}
-
